a1b1125b86764dbb5fc8635d5a4b51be30bb7ff5
Zach <zachrianemachacon@yahoo.com>
2022-01-19 02:58:51 +0800

    Added chromedriver and enabled script to redirect to ISMIS upon execution

--- ismisCrawl.py
+++ ismisCrawl.py
@@ -1,1 +1,7 @@
 # This script will enable us to scrape our grades without opening the ISMIS website.
+
+from selenium import webdriver
+
+browser = webdriver.Chrome('/Users/blurridge/Documents/Coding/ISMISCrawler/chromedriver')
+
+browser.get('https://ismis.usc.edu.ph/Account/Login')

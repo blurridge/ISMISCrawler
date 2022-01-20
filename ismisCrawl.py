be7d70f3c31641f17a4b37cc836e15272704cd3c
Zach <zachrianemachacon@yahoo.com>
2022-01-20 11:16:08 +0800

    Added functions for checking login status and site crash

--- ismisCrawl.py
+++ ismisCrawl.py
@@ -1,4 +1,9 @@
-# This script will enable us to scrape our grades without opening the ISMIS website.
+# /*============================================================================================================
+# FILENAME        :ismisCrawl.py
+# DESCRIPTION     :This script will enable us to scrape our grades without opening the ISMIS website.
+# AUTHOR          :Zach Riane I. Machacon
+# CREATED ON      :20 January 2022
+# ============================================================================================================*/
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -7,11 +12,24 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException     
 import time
 import getpass
 import os
 
-def getUserInput(prompt,maximumNumber=None):
+# Configurations
+
+options = Options()
+options.headless = True # Enables Chromium browser to run without being visible
+ser = Service("/Users/blurridge/Documents/Coding/ISMISCrawler/chromedriver")
+browser = webdriver.Chrome(service = ser, options = options)
+clear = lambda: os.system('clear') # Clears the terminal
+loginStatus = False # Login status initially declared False to initialize next loop of gathering credentials
+homepageCrash = False
+
+# Functions
+
+def getUserInput(prompt, maximumNumber = None):
     if prompt == "What is your username?":
         if maximumNumber is not None:
             while True:
@@ -20,35 +38,57 @@ def getUserInput(prompt,maximumNumber=None):
                     return userInput
                 print("Invalid Input. Username is over 10 characters")
     elif prompt == "What is your password?":
-        return getpass.getpass("What is your password?")
+        return getpass.getpass("What is your password?") # Censors password entry
 
-options = Options()
-options.headless = True # Enables Chromium browser to run without being visible
-ser = Service("/Users/blurridge/Documents/Coding/ISMISCrawler/chromedriver")
-browser = webdriver.Chrome(service = ser, options = options)
+def loginAttempt(username, password):
+    browser.get("https://ismis.usc.edu.ph")
+    username = browser.find_element(By.ID, "Username")
+    password = browser.find_element(By.ID, "Password")
+    loginButton = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    print("Entering username...")
+    username.send_keys(usernameInput)
+    time.sleep(1)
+    print("Entering password...")
+    password.send_keys(passwordInput)
+    time.sleep(1)
+    print(f"Attempting login for {usernameInput}...")
+    loginButton.click()
 
-clear = lambda: os.system('clear') # Clears the terminal
+def checkValidLogin(element):
+    try:
+        browser.find_element(By.CSS_SELECTOR, element)
+    except NoSuchElementException:
+        return True
+    print("Wrong username/password. Please try again.")
+    return False
 
-usernameInput = getUserInput("What is your username?", 10)
-clear()
-passwordInput = getUserInput("What is your password?", None) # Censors password entry
-clear()
+def checkSiteCrash(element):
+    try:
+        browser.find_element(By.ID, element)
+    except NoSuchElementException:
+        return True
+    return False
 
-browser.get("https://ismis.usc.edu.ph")
-username = browser.find_element(By.ID, "Username")
-password = browser.find_element(By.ID, "Password")
-loginButton = browser.find_element(By.CSS_SELECTOR, "button.btn")
-print("Entering username...")
-username.send_keys(usernameInput)
-time.sleep(1)
-print("Entering password...")
-password.send_keys(passwordInput)
-time.sleep(1)
-print(f"Attempting login for {usernameInput}...")
-loginButton.click()
-print("Entering home page...")
+# Main
+
+while(loginStatus == False): # Upon failed login, program resets and asks for user input once again
+    usernameInput = getUserInput("What is your username?", 10)
+    clear()
+    passwordInput = getUserInput("What is your password?", None) 
+    clear()
+    loginAttempt(usernameInput, passwordInput)
+    loginStatus = checkValidLogin("div.validation-summary-errors")
+
+print("Entering home page...") # Enters homepage once loginStatus is set to True
 time.sleep(5)
+homepageCrash = checkSiteCrash("header_profile_pic") # Program then checks for site crash using ISMIS Profile picture as an anchor
+while(homepageCrash == True):
+    print("Site crashed. Refreshing...")
+    browser.refresh()
+    time.sleep(5)
+    homepageCrash = checkSiteCrash("header_profile_pic")
 
+time.sleep(5)
 print("Navigating to grades page...")
 browser.get("https://ismis.usc.edu.ph/ViewGrades")
 
